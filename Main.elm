726052ff73652925daebdbe8aa223db81300c997
John Orford <John.Orford@gmail.com>
2016-04-29 19:19:17 +0200

    layout

--- Main.elm
+++ Main.elm
@@ -1,11 +1,50 @@
+module Main (..) where
 
-import Counter exposing (update, view)
-import StartApp.Simple exposing (start)
+import Router exposing (..)
+import StartApp exposing (App)
+import Effects exposing (Effects, Never)
+import Html exposing (Html)
+import Task exposing (Task)
+import Material.Layout
+import Material.Layout as Layout' exposing (defaultLayoutModel)
+import Layout
 
 
-main =
-  start
-    { model = 0
+init : ( Model, Effects Action )
+init =
+  ( Router.newModel, Effects.none )
+
+
+taggedRouterSignal : Signal Action
+taggedRouterSignal =
+  Signal.map ApplyRoute router.signal
+
+
+app : StartApp.App Model
+app =
+  StartApp.start
+    { init = init
     , update = update
     , view = view
+    , inputs = [ taggedRouterSignal ]
     }
+
+
+inputs : List (Signal.Signal Layout.Action)
+inputs =
+  [ Layout'.setupSignals Layout.LayoutAction ]
+
+
+main : Signal Html
+main =
+  app.html
+
+
+port tasks : Signal (Task.Task Never ())
+port tasks =
+  app.tasks
+
+
+port routeRunTask : Task () ()
+port routeRunTask =
+  router.run
